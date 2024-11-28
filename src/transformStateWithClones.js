a930f7d35b146de308cd4701ec5c26c52b61f072
Uusuff <90629840+Uusuff@users.noreply.github.com>
2024-11-28 12:10:08 +0100

    added task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+
+  for (const i of actions) {
+    if (i.type === 'addProperties') {
+      Object.assign(state, i.extraData);
+    }
+
+    if (i.type === 'removeProperties') {
+      for (const j of i.keysToRemove) {
+        delete state[j];
+      }
+    }
+
+    if (i.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+    stateHistory.push(state);
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
